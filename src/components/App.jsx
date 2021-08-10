fad6d034313cfb6647a72fec00e8d039ec32796c
mehul-clou <85698909+mehul-clou@users.noreply.github.com>
2021-08-10 16:21:22 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Form from "./Form";
+
+var userIsRegistered = false;
+
+function App() {
+  return (
+    <div className="container">
+      <Form 
+      IsRegistered = {userIsRegistered}/>
+    </div>
+  );
+}
+
+export default App;
